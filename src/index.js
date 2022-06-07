595fc921822b857327ff9f6294f40c275b7a8e02
ReadyBlast <88533698+ReadyBlast@users.noreply.github.com>
2022-06-07 16:53:49 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const app = new Application();
+
+console.log(app);
+
+// console.log('Hello World!');
+
+// const mouse = new Mouse(document.body);
+// requestAnimationFrame(tick);
+
+// function tick() {
+//   requestAnimationFrame(tick);
+
+//   console.log(mouse.left, mouse.pLeft);
+
+//   mouse.tick();
+// }
+
